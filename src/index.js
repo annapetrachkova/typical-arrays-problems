92280eabdff35fbc0bb0f3b282cd13894cd3fdad
Анна Петрачкова <p.anytk@mail.ru>
2021-08-05 00:24:39 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length > 0) {
+        return array.reduce(function (p, v) {
+            return (p < v ? p : v);
+        });
+    } else
+        return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+    if (array && array.length > 0) {
+        return array.reduce(function (p, v) {
+            return (p > v ? p : v);
+        });
+    } else
+        return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length > 0) {
+        return array.reduce((sum, item) => (sum += item)) / array.length;
+    } else
+        return 0;
 }
